537e885876af0c0c023f8890fb031a6e344f53bc
seidmengistu <seidmengistu88@gmail.com>
2024-02-20 10:58:57 +0300

    Author controller Api db seed

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function show(Author $author)
+    {
+        //
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PetitionController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/petitions',PetitionController::class);
+
+
+// Route::resource('/petitions', PetitionController::class)->only([
+// 'index','store'
+// ]);
+
+Route::resource('/authors', AuthorController::class)->only([
+'index','show'
+]);
+
