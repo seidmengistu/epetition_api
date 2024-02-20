f5c4f58bc3be2849157fdb73c4f81952c2c8543a
seidmengistu <seidmengistu88@gmail.com>
2024-02-20 10:42:28 +0300

    Seeder and Factory

--- database/factories/PetitionFactory.php
+++ database/factories/PetitionFactory.php
@@ -14,7 +14,12 @@ class PetitionFactory extends Factory
     public function definition()
     {
         return [
-            //
+        "title"=>$this->faker->word,
+        "category"=>$this->faker->text(maxNbChars:50),
+        "description"=>$this->faker->text(200),
+        'author'=>$this->faker->name,
+        'signees'=>$this->faker->numberBetween(0,1000000)
+
         ];
     }
 }
